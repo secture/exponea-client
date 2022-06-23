4f578042318b2545943c75e7291621e312c8a217
pablo-rufat-secture <prufat@secture.com>
2022-06-23 10:14:20 +0200

    fix rollup problem

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
 import dts from 'rollup-plugin-dts';
-export default [
+export default [,
+    {
+        input: 'lib/index.js',
+        output: {
+            file: 'index.js'
+        }
+    },
     {
         input: 'lib/index.d.ts',
         output: {
