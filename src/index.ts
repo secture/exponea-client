42cd36757c6522f7c6cc0b9d5d55af1d4a4c7e4b
pablo-rufat-secture <prufat@secture.com>
2022-07-01 11:09:07 +0200

    chore: leaving just addEvent endpoint

--- src/index.ts
+++ src/index.ts
@@ -44,16 +44,16 @@ export class ExponeaAPI {
     }
   }
 
-  public async updateCustomerProperties(properties: CustomerPropertiesBody): Promise<void> {
-    const response = await this.wrapper.path('/track/v2/projects/{projectToken}/customers', this.projectToken).post({
-      body: properties,
-    });
+  // public async updateCustomerProperties(properties: CustomerPropertiesBody): Promise<void> {
+  //   const response = await this.wrapper.path('/track/v2/projects/{projectToken}/customers', this.projectToken).post({
+  //     body: properties,
+  //   });
 
-    if (response.status !== 200) {
-      response.data.errors.push(response.data.error);
-      throw new APIError(response.status, response.data.errors);
-    }
-  }
+  //   if (response.status !== 200) {
+  //     response.data.errors.push(response.data.error);
+  //     throw new APIError(response.status, response.data.errors);
+  //   }
+  // }
 
   public async addEvent(event: AddEventBody): Promise<void> {
     const response = await this.wrapper
@@ -68,82 +68,82 @@ export class ExponeaAPI {
     }
   }
 
-  public async batchCommand(commands: Command[]): Promise<BatchCommandResponseData> {
-    const response = await this.wrapper.path('/track/v2/projects/{projectToken}/batch', this.projectToken).post({
-      body: {
-        commands,
-      },
-    });
+  // public async batchCommand(commands: Command[]): Promise<BatchCommandResponseData> {
+  //   const response = await this.wrapper.path('/track/v2/projects/{projectToken}/batch', this.projectToken).post({
+  //     body: {
+  //       commands,
+  //     },
+  //   });
 
-    if (response.status !== 200) {
-      response.data.errors.push(response.data.error);
-      throw new APIError(response.status, response.data.errors);
-    } else {
-      return response.data;
-    }
-  }
+  //   if (response.status !== 200) {
+  //     response.data.errors.push(response.data.error);
+  //     throw new APIError(response.status, response.data.errors);
+  //   } else {
+  //     return response.data;
+  //   }
+  // }
 
-  // Customer API
+  // // Customer API
 
-  public async retrieveCustomerAttributes(attributes: CustomerAttributesBody): Promise<AttributeResult[]> {
-    const response = await this.wrapper
-      .path('/data/v2/projects/{projectToken}/customers/attributes', this.projectToken)
-      .post({
-        body: attributes,
-      });
+  // public async retrieveCustomerAttributes(attributes: CustomerAttributesBody): Promise<AttributeResult[]> {
+  //   const response = await this.wrapper
+  //     .path('/data/v2/projects/{projectToken}/customers/attributes', this.projectToken)
+  //     .post({
+  //       body: attributes,
+  //     });
 
-    if (response.status !== 200) {
-      response.data.errors.push(response.data.error);
-      throw new APIError(response.status, response.data.errors);
-    } else {
-      return response.data.results;
-    }
-  }
+  //   if (response.status !== 200) {
+  //     response.data.errors.push(response.data.error);
+  //     throw new APIError(response.status, response.data.errors);
+  //   } else {
+  //     return response.data.results;
+  //   }
+  // }
 
-  public async exporAlltCustomer(exportProperties: ExportAllCustomersBody): Promise<unknown> {
-    const response = await this.wrapper
-      .path('/data/v2/projects/{projectToken}/customers/export', this.projectToken)
-      .post({
-        body: exportProperties,
-      });
+  // public async exporAlltCustomer(exportProperties: ExportAllCustomersBody): Promise<unknown> {
+  //   const response = await this.wrapper
+  //     .path('/data/v2/projects/{projectToken}/customers/export', this.projectToken)
+  //     .post({
+  //       body: exportProperties,
+  //     });
 
-    if (response.status !== 200) {
-      response.data.errors.push(response.data.error);
-      throw new APIError(response.status, response.data.errors);
-    } else {
-      return response.data;
-    }
-  }
+  //   if (response.status !== 200) {
+  //     response.data.errors.push(response.data.error);
+  //     throw new APIError(response.status, response.data.errors);
+  //   } else {
+  //     return response.data;
+  //   }
+  // }
 
-  public async exportCustomer(customerIds: string[]): Promise<ExportCustomerData> {
-    const response = await this.wrapper
-      .path('/data/v2/projects/{projectToken}/customers/export-one', this.projectToken)
-      .post({
-        body: {
-          customer_ids: customerIds,
-        },
-      });
+  // public async exportCustomer(customerIds: string[]): Promise<ExportCustomerData> {
+  //   const response = await this.wrapper
+  //     .path('/data/v2/projects/{projectToken}/customers/export-one', this.projectToken)
+  //     .post({
+  //       body: {
+  //         customer_ids: customerIds,
+  //       },
+  //     });
 
-    if (response.status !== 200) {
-      response.data.errors.push(response.data.error);
-      throw new APIError(response.status, response.data.errors);
-    } else {
-      return response.data;
-    }
-  }
+  //   if (response.status !== 200) {
+  //     response.data.errors.push(response.data.error);
+  //     throw new APIError(response.status, response.data.errors);
+  //   } else {
+  //     return response.data;
+  //   }
+  // }
 
-  public async exportEvent(exportEventProperties: ExportEventsBody): Promise<DataResult[]> {
-    const response = await this.wrapper
-      .path('/data/v2/projects/{projectToken}/customers/events', this.projectToken)
-      .post({
-        body: exportEventProperties,
-      });
+  // public async exportEvent(exportEventProperties: ExportEventsBody): Promise<DataResult[]> {
+  //   const response = await this.wrapper
+  //     .path('/data/v2/projects/{projectToken}/customers/events', this.projectToken)
+  //     .post({
+  //       body: exportEventProperties,
+  //     });
 
-    if (response.status !== 200) {
-      response.data.errors.push(response.data.error);
-      throw new APIError(response.status, response.data.errors);
-    } else {
-      return response.data.data;
-    }
-  }
+  //   if (response.status !== 200) {
+  //     response.data.errors.push(response.data.error);
+  //     throw new APIError(response.status, response.data.errors);
+  //   } else {
+  //     return response.data.data;
+  //   }
+  // }
 }
